2c23bf94837a786404da49b1f3926f843cb877cf
James <dzhem.aptula@gmail.com>
2020-12-30 23:14:58 +0100

    Connection implemented, successfully connects to db and can add bird species, now writing daos

--- birdspotter_2/src/main/java/com/example/birdspotter/BirdspotterApplication.java
+++ birdspotter_2/src/main/java/com/example/birdspotter/BirdspotterApplication.java
@@ -1,5 +1,9 @@
 package com.example.birdspotter;
 
+import domain.BirdSpecie;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -7,9 +11,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 public class BirdspotterApplication {
     public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure()
+                .buildSessionFactory();
+        try {
+            persist(sessionFactory);
+            load(sessionFactory);
+        } finally {
+            sessionFactory.close();
+        }
         SpringApplication.run(BirdspotterApplication.class, args);
+    }
+
+    private static void load(SessionFactory sessionFactory) {
+        System.out.println("-- loading persons --");
+        Session session = sessionFactory.openSession();
+        @SuppressWarnings("unchecked")
+        List<BirdSpecie> persons = session.createQuery("FROM BirdSpecie").list();
+        persons.forEach((x) -> System.out.printf("- %s%n", x));
+        session.close();
+    }
+
+    private static void persist(SessionFactory sessionFactory) {
+        BirdSpecie p1 = new BirdSpecie("Test bird one");
+        BirdSpecie p2 = new BirdSpecie("Test bird two");
+        System.out.println("-- persisting persons --");
+        System.out.printf("- %s%n- %s%n", p1, p2);
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(p1);
+        session.save(p2);
+        session.getTransaction().commit();
     }
 }
--- birdspotter_2/src/main/java/domain/BirdSpecie.java
+++ birdspotter_2/src/main/java/domain/BirdSpecie.java
@@ -1,19 +1,37 @@
 package domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class BirdSpecie {
+	@Id
+	@GeneratedValue
+	public Integer id;
+
 	private String name;
 	
     private Integer yearOfDiscovery;
 	
     private String code;
-		
+
+
 	public BirdSpecie(String name, Integer yearOfDiscovery, String code) {
 		super();
 		this.name = name;
 		this.yearOfDiscovery = yearOfDiscovery;
 		this.code = code;
 	}
-	
+
+	public BirdSpecie(String name) {
+		this.name = name;
+	}
+
+	public BirdSpecie() {
+
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -42,6 +60,4 @@ public class BirdSpecie {
 	public String toString() {
 		return String.format("%s - %d (%s)", getName(), getYearOfDiscovery(), getCode());
 	}
-	
-	
 }
